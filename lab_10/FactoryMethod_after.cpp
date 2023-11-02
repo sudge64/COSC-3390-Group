fbd8dc9ca01f97b8ca676047438a6655256e73fe
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-11-02 15:22:01 -0500

    refactoring

--- lab_10/FactoryMethod_after.cpp
+++ lab_10/FactoryMethod_after.cpp
@@ -8,7 +8,7 @@ public:
     virtual std::string Operation() const = 0;
 };
 
-// Concrete Products: PDF Report and CSV Report
+// Concrete Products: PDF Report 
 class PDFReport : public Report {
 public:
     void generate() override {
@@ -18,6 +18,7 @@ public:
     }
 };
 
+// Concrete Products: CSV Report
 class CSVReport : public Report {
 public:
     void generate() override {
@@ -31,36 +32,20 @@ public:
 class reportFactory : public Report{
     public:
         virtual ~reportFactory(){};
-        virtual Report* FactoryMethod() const = 0;
-        std::string createReport() const {
-            // User input: Choose the type of report to generate
-            std::cout << "Choose the type of report (PDF/CSV): ";
-            std::cin >> reportType;
-
-            if (reportType == "PDF") {
-                // report = new PDFReport();// instead, assign to factory a new PDFReportFactory
-                factory = new PDFReportFactory();// instead, assign to factory a new PDFReportFactory
-            } else if (reportType == "CSV") {
-                // report = new CSVReport();// instead, assign to factory a new PDFReportFactory
-                factory = new CSVReportFactory();// instead, assign to factory a new PDFReportFactory
-            } else {
-                std::cout << "Invalid report type." << std::endl;
-                return 1;
-            }
-        }
+        virtual Report* createReport() const = 0;
 };
 
 // Concrete Factories: PDFReportFactory and CSVReportFactory
 class PDFReportFactory : public reportFactory{
     public:
-        Report* FactoryMethod() const override{
+        Report* createReport() const override{
             return new PDFReportFactory();
         }
 };
 
 class CSVReportFactory : public reportFactory{
     public:
-        Report* FactoryMethod() const override{
+        Report* createReport() const override{
             return new CSVReportFactory();
         }
 };
@@ -68,10 +53,10 @@ class CSVReportFactory : public reportFactory{
 int main() {
     // Client code
     std::string reportType;
-	Report *report;
-	// Create "factory" pointer to ReportFactory
+    Report *report;
+    // Create "factory" pointer to ReportFactory
     reportFactory *factory = new reportFactory();
-    /*
+    
     // User input: Choose the type of report to generate
     std::cout << "Choose the type of report (PDF/CSV): ";
     std::cin >> reportType;
@@ -86,16 +71,16 @@ int main() {
         std::cout << "Invalid report type." << std::endl;
         return 1;
     }
-*/
-	// report must receive return from method "factory->createReport()"
-        report = factory->createReport();
+
+    // report must receive return from method "factory->createReport()"
+    report = factory->createReport();
 
     // Generate the selected report
     report->generate();
 
     // Clean up
     delete report;
-	// Also delete factory
+    // Also delete factory
     delete factory;
     return 0;
 }
