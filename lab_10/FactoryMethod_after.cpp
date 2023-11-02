5c0bab5764266b83c17bb20721c8f22b8fc9cd6f
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-11-02 14:36:25 -0500

    start refactoring

--- lab_10/FactoryMethod_after.cpp
+++ lab_10/FactoryMethod_after.cpp
@@ -24,47 +24,54 @@ public:
     }
 };
 
-//// Abstract Factory: ReportFactory
-class reportFactory{
+// Abstract Factory: ReportFactory
+class reportFactory : public Report{
+    public:
+        void generate() override{
+            
+        }
 
 };
 
-////Concrete Factories: PDFReportFactory and CSVReportFactory
+// Concrete Factories: PDFReportFactory and CSVReportFactory
 class PDFReportFactory : public reportFactory{
-
+    public:
+        PDFReport::generate();
 };
 
 class CSVReportFactory : public reportFactory{
-
+    public:
+        CSVReport::generate();
 };
 
 int main() {
     // Client code
     std::string reportType;
 	Report *report;
-	////Create "factory" pointer to ReportFactory
+	// Create "factory" pointer to ReportFactory
+    reportFactory *factory = new reportFactory();
 
     // User input: Choose the type of report to generate
     std::cout << "Choose the type of report (PDF/CSV): ";
     std::cin >> reportType;
 
     if (reportType == "PDF") {
-        report = new PDFReport();////instead, assign to factory a new PDFReportFactory
+        report = new PDFReport();// instead, assign to factory a new PDFReportFactory
     } else if (reportType == "CSV") {
-        report = new CSVReport();////instead, assign to factory a new PDFReportFactory
+        report = new CSVReport();// instead, assign to factory a new PDFReportFactory
     } else {
         std::cout << "Invalid report type." << std::endl;
         return 1;
     }
 
-	////report must receive return from method "factory->createReport()"
+	// report must receive return from method "factory->createReport()"
 
     // Generate the selected report
     report->generate();
 
     // Clean up
     delete report;
-	////Also delete factory
-
+	// Also delete factory
+    delete factory;
     return 0;
 }
