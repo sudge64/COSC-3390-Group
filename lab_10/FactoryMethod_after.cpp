9a6767ccdb96a463b3f97052283cb79a6b91de66
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-11-02 14:51:47 -0500

    refactoring

--- lab_10/FactoryMethod_after.cpp
+++ lab_10/FactoryMethod_after.cpp
@@ -5,6 +5,7 @@
 class Report {
 public:
     virtual void generate() = 0;
+    virtual std::string Operation() const = 0;
 };
 
 // Concrete Products: PDF Report and CSV Report
@@ -27,21 +28,39 @@ public:
 // Abstract Factory: ReportFactory
 class reportFactory : public Report{
     public:
-        void generate() override{
-            
-        }
+        virtual ~reportFactory(){};
+        virtual Report* FactoryMethod() const = 0;
+        std::string createReport() const {
+            // User input: Choose the type of report to generate
+            std::cout << "Choose the type of report (PDF/CSV): ";
+            std::cin >> reportType;
 
+            if (reportType == "PDF") {
+                // report = new PDFReport();// instead, assign to factory a new PDFReportFactory
+                factory = new PDFReportFactory();// instead, assign to factory a new PDFReportFactory
+            } else if (reportType == "CSV") {
+                // report = new CSVReport();// instead, assign to factory a new PDFReportFactory
+                factory = new CSVReportFactory();// instead, assign to factory a new PDFReportFactory
+            } else {
+                std::cout << "Invalid report type." << std::endl;
+                return 1;
+            }
+        }
 };
 
 // Concrete Factories: PDFReportFactory and CSVReportFactory
 class PDFReportFactory : public reportFactory{
     public:
-        PDFReport::generate();
+        Report* FactoryMethod() const override{
+            return new PDFReportFactory();
+        }
 };
 
 class CSVReportFactory : public reportFactory{
     public:
-        CSVReport::generate();
+        Report* FactoryMethod() const override{
+            return new CSVReportFactory();
+        }
 };
 
 int main() {
@@ -50,21 +69,24 @@ int main() {
 	Report *report;
 	// Create "factory" pointer to ReportFactory
     reportFactory *factory = new reportFactory();
-
+    /*
     // User input: Choose the type of report to generate
     std::cout << "Choose the type of report (PDF/CSV): ";
     std::cin >> reportType;
 
     if (reportType == "PDF") {
-        report = new PDFReport();// instead, assign to factory a new PDFReportFactory
+        // report = new PDFReport();// instead, assign to factory a new PDFReportFactory
+        factory = new PDFReportFactory();// instead, assign to factory a new PDFReportFactory
     } else if (reportType == "CSV") {
-        report = new CSVReport();// instead, assign to factory a new PDFReportFactory
+        // report = new CSVReport();// instead, assign to factory a new PDFReportFactory
+        factory = new CSVReportFactory();// instead, assign to factory a new PDFReportFactory
     } else {
         std::cout << "Invalid report type." << std::endl;
         return 1;
     }
-
+*/
 	// report must receive return from method "factory->createReport()"
+        report = factory->createReport();
 
     // Generate the selected report
     report->generate();
