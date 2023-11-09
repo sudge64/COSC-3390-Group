fb77dce06bd0a5b03060b0cd17e04f3f685a7cb0
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-11-09 14:46:37 -0600

    add other file

---
+++ lab_11/ProcessOrder_after.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+class Order {
+public:
+    void processOrder(int productId, int quantity) {
+        // Fetch product details from the database
+        // ...
+
+        // Validate the order quantity
+        if (quantity <= 0) {
+            std::cout << "Invalid quantity. Order not processed." << std::endl;
+            return;
+        }
+
+        // Calculate the total price
+        double price = calculatePrice(productId, quantity);
+
+        // Charge the customer
+        chargeCustomer(price);
+
+        // Update inventory
+        updateInventory(productId, quantity);
+
+        // Send order confirmation
+        sendConfirmation();
+    }
+
+private:
+    double calculatePrice(int productId, int quantity) {
+        // Fetch product price from the database
+        // ...
+
+        // Calculate the total price
+        double price = 1.5f * quantity; /* some calculation */;
+        return price;
+    }
+
+    void chargeCustomer(double amount) {
+        // Implement logic to charge the customer
+        // ...
+        std::cout << "Customer charged: $" << amount << std::endl;
+    }
+
+    void updateInventory(int productId, int quantity) {
+        // Implement logic to update inventory
+        // ...
+        std::cout << "Inventory updated for product " << productId << std::endl;
+    }
+
+    void sendConfirmation() {
+        // Implement logic to send order confirmation
+        // ...
+        std::cout << "Order confirmation sent" << std::endl;
+    }
+};
+
+int main() {
+    Order myOrder;
+    int productID, quantity;
+    // myOrder.processOrder(1, 3);
+    myOrder.processOrder(productID, quantity);
+
+    return 0;
+}
