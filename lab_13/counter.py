053428275b2b83f7c5faa9512776181ac0b3cd69
C.J. Wade <95940433+sudge64@users.noreply.github.com>
2023-11-30 14:51:23 -0600

    Add python program

---
+++ lab_13/counter.py
@@ -0,0 +1,99 @@
+"""
+This program is for taking in a C++ file,
+counting the classes, lines, and comments, and outputing the results
+"""
+import sys
+import getopt
+import re
+
+
+def open_file(file_input):
+    """
+    Function to open a file and save each line to a list
+    """
+    with open(file_input, 'r', encoding='UTF-8') as file_one:
+        list_of_lines = file_one.readlines()
+    file_one.close()
+
+    num_lines = 0
+    num_class = 0
+    num_comments = 0
+    for current in list_of_lines:
+        # print(F"Line {num_lines}: {current}", end="")
+        if re.search('^class.*', current):
+            # print("FOUND! Clacc")
+            class_counter(current, list_of_lines, num_lines)
+            num_class += 1
+        if re.search(' *//.*', current):
+            # print("FOUND! Comment")
+            num_comments += 1
+            num_lines -= 1
+        if re.search('^\s*$', current):
+            # print("FOUND! Nothing!")
+            num_lines -= 1
+        if re.search('^int main.*', current):
+            print("FOUND! Main")
+        num_lines += 1
+
+    comment_density = (num_comments / num_lines) * 100
+    line_average = (num_comments / num_lines) * 100
+    comment_average = (num_comments / num_lines) * 100
+
+    print("\nTotals:\n")
+    print(F"\tClasses:\t\t{num_class}")
+    print(F"\tLines Count:\t\t{num_lines}")
+    print(F"\tComments Count:\t\t{num_comments}")
+    print(F"\tComments density:\t{comment_density} %")
+    print(F"\tLines Avg:\t\t{line_average}")
+    print(F"\tComments Avg:\t\t{comment_average}")
+
+
+def class_counter(current, list_of_lines, num_lines):
+    """
+    Function to count lines inside of classes
+    """
+    class_name = current
+    class_lines = 0
+    class_comments = 0
+
+    for current in list_of_lines:
+        # print(F"Line {num_lines}: {current}", end="")
+        if re.search(' *//.*', current):
+            # print("FOUND! Commentj")
+            class_comments += 1
+            class_lines -= 1
+        if re.search('^\s*$', current):
+            # print("FOUND! Nothing!")
+            class_lines -= 1
+        if re.search('^};*', current):
+            # print("FOUND! End of class")
+            break
+        class_lines += 1
+
+    comment_density = (class_comments / class_lines) * 100
+    print(F"\n{class_name}")
+    print(F"\tLines Count: {class_lines}", end="")
+    print(F", Comments Count: {class_comments}", end="")
+    print(F", Comments density: {comment_density} %")
+
+
+def main(argv):
+    """
+    Main Function, handles reading arguments passed in from command line
+    """
+    input_file = ''
+    opts, arg = getopt.getopt(argv, "hi:", ["ifile="])
+    for opt, arg in opts:
+        if opt == '-h':
+            print('<filename>.py -i <inputfile>')
+            sys.exit()
+        elif opt in ("-i", "--ifile"):
+            input_file = arg
+        else:
+            print('Correct usage is: <filename>.py -i <inputfile>')
+    print('Input file is', input_file)
+    open_file(input_file)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
